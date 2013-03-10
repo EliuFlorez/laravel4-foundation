3215e44b31e73cdf0f798e57e0c98737f8dfe9c8
Jake Chapman <jake@imjakechapman.com>
2013-03-10 04:56:41 -0700

    removed database info from app/config/database.php

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'trackd_dev',
-			'username'  => 'root',
-			'password'  => 'root',
+			'database'  => 'YOURDATABASE',
+			'username'  => 'mostLikelyRoot',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
